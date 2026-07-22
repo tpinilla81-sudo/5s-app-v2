40ac09276b962d2f9a5c056c5a537b4fdcabb664
Z User <z@container>
2026-07-22 07:37:16 +0000

    Clean repo v2: remove config files, logs, deploy scripts

--- daemon.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-cd /home/z/my-project
-export DATABASE_URL="file:/home/z/my-project/db/custom.db"
-export PORT=3000
-export HOSTNAME=0.0.0.0
-
-# Double fork to fully detach from controlling terminal
-(setsid npx next start -p 3000 > /tmp/nextdaemon.log 2>&1 &)
