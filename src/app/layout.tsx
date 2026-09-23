5f7e025a0388a7a63fbc39fd6c1741033e487723
t-pinilla-8181 <t_pinilla@outlook.com>
2026-09-23 20:58:38 +0000

    4b4db4cc-5f47-4e0b-b3c2-020e4679bbe2

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -122,8 +122,8 @@ export default function RootLayout({
           dangerouslySetInnerHTML={{
             __html: `
               (function() {
-                // v3.0.27 - VER Y RESETEAR CONTRASEÑA ADMIN
-                var CURRENT_VERSION = 'v3.0.32';
+                // v3.0.44 - FIX: Plantillas públicas + Añadir usuarios existentes
+                var CURRENT_VERSION = 'v3.0.44';
                 
                 // Check if we need to force reload
                 var storedVersion = localStorage.getItem('_app_force_version');
