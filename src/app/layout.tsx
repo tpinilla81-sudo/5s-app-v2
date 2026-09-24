f74d0aa0fd1f03555ccf1925bb437389515ea6c4
t-pinilla-8181 <t_pinilla@outlook.com>
2026-09-24 16:31:07 +0000

    6bec22b8-9158-456b-9cdc-2b66ad5ba8c5

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -122,8 +122,8 @@ export default function RootLayout({
           dangerouslySetInnerHTML={{
             __html: `
               (function() {
-                // v3.0.50 - ACTUALIZACIÓN CRÍTICA FORZADA
-                var CURRENT_VERSION = 'v3.0.50';
+                // v3.0.51 - FIX: Forzar actualización desde v3.0.43
+                var CURRENT_VERSION = 'v3.0.51';
                 
                 // Check if we need to force reload
                 var storedVersion = localStorage.getItem('_app_force_version');
